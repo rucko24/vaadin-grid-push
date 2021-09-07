cfab465a12cd214d64c336d5a11777994d36616b
rucko24 <rucko24@gmail.com>
2021-09-07 20:36:22 +0200

    Remove CountDownLatch

--- src/main/java/com/example/application/reactivedatabase/service/ReactiveBookService.java
+++ src/main/java/com/example/application/reactivedatabase/service/ReactiveBookService.java
@@ -2,9 +2,9 @@ package com.example.application.reactivedatabase.service;
 
 import com.example.application.reactivedatabase.model.Book;
 import com.example.application.reactivedatabase.repository.BookReactiveRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,16 +12,12 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 
-@Service
+@RequiredArgsConstructor
+@Component
 @Log4j2
 public class ReactiveBookService {
 
-    private BookReactiveRepository bookReactiveRepository;
-
-    @Autowired
-    public ReactiveBookService(final BookReactiveRepository bookReactiveRepository) {
-        this.bookReactiveRepository = bookReactiveRepository;
-    }
+    private final BookReactiveRepository bookReactiveRepository;
 
     public Mono<Mono<Book>> findById(final String id) {
         return Mono.justOrEmpty(bookReactiveRepository.findById(id));
@@ -48,29 +44,30 @@ public class ReactiveBookService {
     }
 
     public Flux<List<Book>> findAll() {
-        final CountDownLatch latch = new CountDownLatch(1);
         final List<Book> bookList = new CopyOnWriteArrayList<>();
-        bookReactiveRepository.findAll()
-                .doOnComplete(latch::countDown)
-                .subscribe(bookList::add);
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return Flux.just(bookList);
+        return bookReactiveRepository.findAll()
+                .flatMap((Book book) -> {
+                    bookList.add(book);
+                    return Flux.just(bookList);
+                });
     }
 
+    /**
+     * Add countDownLatch with subscription
+     *
+     * @return List<Book>
+     */
     public List<Book> findAllForUpdatedBooks() {
         final CountDownLatch latch = new CountDownLatch(1);
         final List<Book> bookList = new CopyOnWriteArrayList<>();
         bookReactiveRepository.findAll()
                 .doOnComplete(latch::countDown)
+                .doOnError(Flux::error)
                 .subscribe(bookList::add);
         try {
             latch.await();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.error("Error findAllForUpdatedBooks() {}", e);
         }
         return bookList;
     }
